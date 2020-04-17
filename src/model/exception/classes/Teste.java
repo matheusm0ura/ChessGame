60e3350cf6e8fa6a339c678bf3af7f32bde83c23
matheusm0ura <matheusdj49@live.com>
2020-04-17 18:20:29 -0300

    Inicial commit

--- src/model/exception/classes/Teste.java
+++ src/model/exception/classes/Teste.java
@@ -1,4 +1,4 @@
-package model.exception;
+package model.exception.classes;
 
 public class Teste {
     public static void main(String[] args) {
---
+++ src/model/exception/teste/teste2.java
@@ -0,0 +1,7 @@
+package model.exception.teste;
+
+public class teste2 {
+    public static void main(String[] args) {
+        System.out.println("asdasd");
+    }
+}
