9f2a2b17ddf06fff33a36fdfd6d9d1a4d5e86b45
matheusm0ura <matheusdj49@live.com>
2020-04-17 21:05:10 -0300

    Board and Piece

---
+++ src/application/Program.java
@@ -0,0 +1,9 @@
+package application;
+
+import boardgame.Board;
+
+public class Program {
+    public static void main(String[] args) {
+
+    }
+}
--- src/application/classes/Teste.java
+++
@@ -1,7 +0,0 @@
-package application.classes;
-
-public class Teste {
-    public static void main(String[] args) {
-        System.out.print("OlA");
-    }
-}
